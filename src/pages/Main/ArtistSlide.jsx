d525fc2292abfdd468d62c29901a183c7828edb5
kravchenko-d <kravchenko4@mail.ru>
2024-10-16 12:13:52 +0300

    refactor: save spaces [DID-16]

--- src/pages/Main/ArtistSlide.jsx
+++ src/pages/Main/ArtistSlide.jsx
@@ -32,37 +32,37 @@ const ArtistSlide = () => {
   }, [activeIndex]);
 
   return (
-    <section className="carousel">
-      <div className="carousel__inner">
-        {randomArtists.map((artist, index) => (
-          <div
-            key={index}
-            className={`dj ${index === activeIndex ? 'active' : ''}`}
-            onClick={index === activeIndex ? () => handleNavArtist(artist.category) : null}>
-            <div className="dj__wrap">
-              <img src={artist.image} alt="Slide" className="dj__image" />
-              <div className="figure dj__figure"></div>
-            </div>
-            <div className="dj__content">
-              <h3 className="title dj__title">About {artist.name}</h3>
-              <p className="dj__text">{artist.shortDescription}</p>
-              <p className="dj__quote">
-                I try to experiment with various music genres in every track that I make.
-              </p>
-              <p className="dj__text" id="text">
-                {artist.fullDescription}
-              </p>
+      <section className="carousel">
+        <div className="carousel__inner">
+          {randomArtists.map((artist, index) => (
+            <div
+              key={index}
+              className={`dj ${index === activeIndex ? 'active' : ''}`}
+              onClick={index === activeIndex ? () => handleNavArtist(artist.category) : null}>
+              <div className="dj__wrap">
+                <img src={artist.image} alt="Slide" className="dj__image" />
+                <div className="figure dj__figure"></div>
+              </div>
+              <div className="dj__content">
+                <h3 className="title dj__title">About {artist.name}</h3>
+                <p className="dj__text">{artist.shortDescription}</p>
+                <p className="dj__quote">
+                  I try to experiment with various music genres in every track that I make.
+                </p>
+                <p className="dj__text" id="text">
+                  {artist.fullDescription}
+                </p>
             </div>
           </div>
-        ))}
-      </div>
-      <div className="arrow left-arrow" onClick={handlePrev}>
-        ←
-      </div>
-      <div className="arrow right-arrow" onClick={handleNext}>
-        →
-      </div>
-    </section>
+          ))}
+        </div>
+        <div className="arrow left-arrow" onClick={handlePrev}>
+          ←
+        </div>
+        <div className="arrow right-arrow" onClick={handleNext}>
+          →
+        </div>
+      </section>
   );
 };
 
