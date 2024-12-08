6f147dbd3c145d05535acb95fdb5984f3ea20d79
mikhai.palienko <mikhai.palienko@aliexpress.ru>
2024-12-08 21:13:34 +0300

    text

--- src/pages/Main/ServicesSection/ServiceItem/index.jsx
+++ src/pages/Main/ServicesSection/ServiceItem/index.jsx
@@ -1,3 +1,4 @@
+import styles from "./ServiceItem.module.scss";
 const ServiceItem = ({ service, length, onOpenModal }) => {
   return (
     <>
@@ -5,13 +6,14 @@ const ServiceItem = ({ service, length, onOpenModal }) => {
         <h3 className="tickets__subtitle">{service.title}</h3>
         <span className="tickets__price">{service.price}</span>
         <div className="tickets__text-cont">
-          <span className="tickets__text">1 Event</span>
-          <span className="tickets__text">Basic Access</span>
-          <span className="tickets__text">Club Events Only</span>
-          <span className="tickets__text">Free Snacks</span>
+          {service.text.map((item, i) => (
+            <span className="tickets__text" key={i}>
+              {item}
+            </span>
+          ))}
         </div>
 
-        <button className="button button-secondary" onClick={onOpenModal}>
+        <button className="button button-secondary tickets__button" onClick={onOpenModal}>
           Заказать
         </button>
       </div>
